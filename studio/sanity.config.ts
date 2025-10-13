893277ffe9673943d2021c7a1bcee8852289f145
Jose Ramirez <galock@bu.edu>
2025-10-13 17:05:30 -0400

    Debugging deployment

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
+// import {structureTool} from "sanity/structure"
 import { visionTool } from "@sanity/vision";
 import { schema } from "./schemaTypes";
 
 export default defineConfig({
   name: "default",
   title: "Studio",
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID!, // ← use SANITY_STUDIO_*
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
   dataset: process.env.SANITY_STUDIO_DATASET!,
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    // structureTool({ structure: undefined }), // TEMP: comment out/remove
+    visionTool(),
+  ],
   schema,
 });
