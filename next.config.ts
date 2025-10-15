467faa06045e3608b18b8247725c93e16c56e329
Jose Ramirez <galock@bu.edu>
2025-10-15 17:23:09 -0400

    fixed bug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: ["sanity", "next-sanity", "@sanity/vision"],
   experimental: { externalDir: true }, // allows ../ imports
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
+
 export default nextConfig;
